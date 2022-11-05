dab2f701de44fc8db984971673b106c0744867fc
Jaroslavs Samcuks <yarcat@gmail.com>
2022-11-05 09:19:02 +0100

    list: add some tests

--- list/list_test.go
+++ list/list_test.go
@@ -1,7 +1,63 @@
 package list
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
 
 func TestList(t *testing.T) {
-	t.Error("must be implemented")
+	for _, tc := range []struct {
+		name         string
+		initF, wantF func() List
+		comp         func(a, b List) bool
+	}{
+		{"Len0", list(), dontCare, checkLen(0)},
+		{"Len5", list(1, 2, 3, 4, 5), dontCare, checkLen(5)},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.initF()
+			want := tc.wantF()
+			if !tc.comp(got, want) {
+				t.Errorf("got = %v, want = %v", format(got), format(want))
+			}
+		})
+	}
+}
+
+func list(e ...int) func() List {
+	return func() (l List) {
+		for i := range e {
+			l.PushBack(e[i])
+		}
+		fmt.Println(format(l))
+		return l
+	}
+}
+
+func dontCare() List { return List{} }
+
+func equal(a, b List) bool {
+	if a.Len() != b.Len() {
+		return false
+	}
+	for a, b := a.Front(), b.Front(); a != nil; a, b = a.Next(), b.Next() {
+		if a.Value != b.Value {
+			return false
+		}
+	}
+	return true
+}
+
+func checkLen(want uint) func(a, _ List) bool {
+	return func(a, _ List) bool { return a.Len() == want }
+}
+
+func format(l List) string {
+	var b bytes.Buffer
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Fprintf(&b, "->[%v]", e.Value)
+	}
+	b.WriteString("->|")
+	return b.String()
 }
